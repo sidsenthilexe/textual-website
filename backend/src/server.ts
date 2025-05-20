f45fff54fd78f1735516f4f569709b1b0fc366a2
Sid Senthil <sidsenthil2010@gmail.com>
2025-05-19 21:59:01 -0700

    update 1 file, create 1 file and move 7 files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,10 +5,18 @@ dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 3000;
+const TS_API_KEY = process.env.TS_API_KEY;
 
 server.get("/", (_req, res) => {
     res.send("Running :)")
 });
+
+server.get("/tailscale/devices", async (_req, res) => {
+    if (!TS_API_KEY) {
+        return res.status(500).send("API Key not set");
+    }
+    const response = await fetch("")
+})
 
 server.listen(PORT, () => {
     console.log(`running on port ${PORT}`)
